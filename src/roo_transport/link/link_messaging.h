edb239c46647fd302eb70455c352e1ab28f59cb7
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-09-29 22:36:34 +0200

    Fixed concurrency issues in link messaging.

--- src/roo_transport/link/link_messaging.h
+++ src/roo_transport/link/link_messaging.h
@@ -5,6 +5,7 @@
 #include "roo_io/memory/store.h"
 #include "roo_io/status.h"
 #include "roo_logging.h"
+#include "roo_threads/mutex.h"
 #include "roo_transport/link/link_transport.h"
 #include "roo_transport/messaging/messaging.h"
 #include "roo_transport/socket_input_stream.h"
@@ -26,20 +27,22 @@ class LinkMessaging : public Messaging {
   void begin() { connect(); }
 
   void send(const void* data, size_t size) override {
+    connect();
+    SocketOutputStream& out = this->out();
     // if (socket_.isConnecting()) {
     //   LOG(INFO) << "Still connecting; await";
     //   socket_.awaitConnected();
     //   LOG(INFO) << "Connected";
     // }
-    if (out().status() != roo_io::kOk) {
-      LOG(INFO) << out().status();
-      connect();
-    }
+    // if (out.status() != roo_io::kOk) {
+    //   LOG(INFO) << out.status();
+    //   connect();
+    // }
     roo::byte header[4];
     roo_io::StoreBeU32(size, header);
-    out().writeFully(header, 4);
-    out().writeFully((const roo::byte*)data, size);
-    out().flush();
+    out.writeFully(header, 4);
+    out.writeFully((const roo::byte*)data, size);
+    out.flush();
   }
 
   void registerRecvCallback(
@@ -49,24 +52,40 @@ class LinkMessaging : public Messaging {
 
  private:
   void connect() {
-    link_ = transport_.connectAsync();
-    in().onReceive([this]() { received(); });
+    roo::lock_guard<roo::mutex> guard(mutex_);
+    while (true) {
+      LinkStatus status = link_.status();
+      if (status == LinkStatus::kConnected ||
+          status == LinkStatus::kConnecting) {
+        // Already connected or in the process of connecting.
+        break;
+      }
+      link_ = transport_.connectAsync();
+      link_.in().onReceive([this]() { received(); });
+    }
   }
 
-  roo_transport::SocketInputStream& in() { return link_.in(); }
+  roo_transport::SocketInputStream& in() {
+    roo::lock_guard<roo::mutex> guard(mutex_);
+    return link_.in();
+  }
 
-  roo_transport::SocketOutputStream& out() { return link_.out(); }
+  roo_transport::SocketOutputStream& out() {
+    roo::lock_guard<roo::mutex> guard(mutex_);
+    return link_.out();
+  }
 
   void received() {
+    SocketInputStream& in = this->in();
     while (true) {
       if (incoming_size_ < 0) {
         // Header not yet fully read.
-        pos_ += in().tryRead(incoming_header_ + pos_, 4 - pos_);
+        pos_ += in.tryRead(incoming_header_ + pos_, 4 - pos_);
         if (pos_ < 4) {
           // Still incomplete.
-          if (in().status() != roo_io::kOk) {
+          if (in.status() != roo_io::kOk) {
             pos_ = 0;
-            LOG(ERROR) << "Error: " << in().status();
+            LOG(ERROR) << "Error: " << in.status();
             connect();
           }
           return;
@@ -77,12 +96,12 @@ class LinkMessaging : public Messaging {
       }
       while (pos_ < incoming_size_) {
         size_t read =
-            in().tryRead(incoming_payload_.get() + pos_, incoming_size_ - pos_);
+            in.tryRead(incoming_payload_.get() + pos_, incoming_size_ - pos_);
         if (read == 0) {
-          if (in().status() != roo_io::kOk) {
+          if (in.status() != roo_io::kOk) {
             pos_ = 0;
             incoming_size_ = -1;
-            LOG(ERROR) << "Error: " << in().status();
+            LOG(ERROR) << "Error: " << in.status();
             connect();
           }
           return;
@@ -106,6 +125,8 @@ class LinkMessaging : public Messaging {
   int incoming_size_;
   int pos_;
   std::unique_ptr<roo::byte[]> incoming_payload_;
+
+  mutable roo::mutex mutex_;
 };
 
 }  // namespace roo_transport
