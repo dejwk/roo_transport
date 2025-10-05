13c1069458d2cbfdef8121aca0be35222ef094a3
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-10-05 20:18:39 +0200

    Removing 'onReceive' from the input stream. One day this could be replaced by select functionality?

--- src/roo_transport/socket_input_stream.h
+++ src/roo_transport/socket_input_stream.h
@@ -8,10 +8,6 @@ namespace roo_transport {
 
 class SocketInputStream : public roo_io::InputStream {
  public:
-  using ReceiveCb = std::function<void()>;
-
-  virtual void onReceive(ReceiveCb recv_cb) = 0;
-
   virtual size_t available() = 0;
   virtual int read() = 0;
   virtual int peek() = 0;
